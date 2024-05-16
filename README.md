e5994b17bfdd2ee9c84298b886808fa240875068
Pranaav Honnavalli <pranaav.honnavalli@briq.com>
2024-05-16 18:32:55 +0200

    Add instructions to run the web application, load balancer, autoscaler, and testing in README.md

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
+<!-- Getting started -->
+# Getting started
+This is an autoscalar for a web application running as a podman container. It is designed to automatically scale the number of contaienrs based on the number of requests to the application.
 
+## Prerequisites
+- Podman
+
+## Web application
+The web application is a simple Python Flask application that returns a JSON response with the multiple API endpoints. The application is running on port 5000.
+<!-- List steps to run the webapp as a podman container -->
+## Steps to run the webapp as a podman container
+1. Clone the repository
+2. Build the container image in the webapp directory with the following command
+```podman build -t webapp .```
+3. Run the container with the following command
+```podman run -d -p 5000:5000 -v /Users/pradeep/Desktop/LeidenUni/sem2/CC/scaling-controller/webapp/data:/app/data --name webapp1 webapp```
+
+## Load Balancer
+We make use of a HAProxy load balancer to distribute the incoming requests to the web application containers. 
+
+<!-- List steps to run the load balancer as a podman container -->
+## Steps to run the load balancer as a podman container
+1. Clone the repository
+2. Go to the haproxy direcctory in the repository
+3. Build the container image with the following command
+```podman build -t haproxy .```
+4. Run the container with the following command
+```podman run -d --restart always --name haproxy -p 8080:8080 -p 9999:9999 -p 8888:8888 -v /Users/pradeep/Desktop/LeidenUni/sem2/CC/scaling-controller/haproxy/conf:/etc/haproxy  haproxy```
+
+## Autoscaler
+These are the steps to run the autoscaler as a podman container
+1. Clone the repository
+2. Go to the autoscaler directory in the repository
+3. Run the python script with the following command
+```python3 autoscaler.py```
+
+## Testing
+To test the autoscaler, we can use the Locust tool to generate load on the web application. 
+1. Install Locust with the following command
+```pip install locust```
+2. Run the Locust tool with the following command
+```locust -f locustfile.py```
+3. Open the browser and go to the URL: http://localhost:8089
+4. Enter the number of users and the hatch rate and click on the start button to start the test.
+
+## Authors
+- Honnavalli Pranaav
--- scaling-controller/scaler.py
+++ scaling-controller/scaler.py
@@ -1,1 +1,61 @@
+import time
+import requests
+import subprocess
 
+# HAProxy stats endpoint URL
+stats_url = "http://localhost:8080/stats"
+
+# Scaling thresholds
+scale_up_threshold = 20
+scale_down_threshold = 10
+
+# Container management commands
+start_container_cmd = "podman run -d webapp"
+stop_container_cmd = "podman stop {}"
+
+# Function to scale up containers
+def scale_up_containers(num_containers):
+    for _ in range(num_containers):
+        subprocess.run(start_container_cmd, shell=True)
+
+# Function to scale down containers
+def scale_down_containers(num_containers):
+    containers = subprocess.check_output("podman ps -q", shell=True).decode().splitlines()
+    containers_to_stop = containers[:num_containers]
+    for container in containers_to_stop:
+        subprocess.run(stop_container_cmd.format(container), shell=True)
+
+# Function to check activity and bring down idle containers
+def check_activity():
+    containers = subprocess.check_output("podman ps -q", shell=True).decode().splitlines()
+    active_containers = []
+    for container in containers:
+        container_stats_url = f"http://{container}:8080/stats"
+        try:
+            response = requests.get(container_stats_url)
+            if response.status_code == 200:
+                active_containers.append(container)
+        except requests.exceptions.RequestException:
+            pass
+    if len(active_containers) > 1:
+        containers_to_stop = active_containers[1:]
+        for container in containers_to_stop:
+            subprocess.run(stop_container_cmd.format(container), shell=True)
+
+# Main loop
+while True:
+    try:
+        response = requests.get(stats_url)
+        if response.status_code == 200:
+            stats = response.json()
+            total_requests = int(stats["total_requests"])
+            containers = subprocess.check_output("podman ps -q", shell=True).decode().splitlines()
+            num_containers = len(containers)
+            if total_requests > scale_up_threshold * num_containers:
+                scale_up_containers(1)
+            elif total_requests < scale_down_threshold * num_containers and num_containers > 1:
+                scale_down_containers(1)
+            check_activity()
+    except requests.exceptions.RequestException:
+        pass
+    time.sleep(15)
